b4afc1a84cc4ed45dc0a3a0c192837d3f3944028
Fizal Sarif <fizal.sarif@gmail.com>
2016-08-11 22:22:41 -0400

    delete button

--- src/client/app/Dashboard.jsx
+++ src/client/app/Dashboard.jsx
@@ -27,7 +27,35 @@ class Dashboard extends React.Component{
   // My Code
   //========================================
 
+  //DELETE
+  delete(applicationID, stateID){
+    // try to get the token from the sessionStorage
+    let token = JSON.parse(sessionStorage.getItem('intview'))['access_token']
 
+    var myHeaders = new Headers()
+
+    myHeaders.append('Content-Type', 'application/json')
+    myHeaders.append('Authorization', token)
+    myHeaders.append('Origin', 'http://localhost:4000') //define Origin or the server WILL reject the message.
+
+    fetch('http://localhost:3000/applications/'+applicationID, {
+      method:'DELETE',
+      headers: myHeaders,
+      // mode:'no-cors'
+    })
+    .then(r=>r.json())
+    .then(data=>{
+      console.log(data)
+      this.state.applications.splice(stateID, 1)
+      this.setState({
+        applications: this.state.applications
+      })
+    })
+  }
+
+  //========================================
+
+  //GET ALL
   getApplications(){
     // try to get the token from the sessionStorage
     let token = JSON.parse(sessionStorage.getItem('intview'))['access_token']
@@ -60,13 +88,14 @@ class Dashboard extends React.Component{
       <div>
         <h1>Dashboard</h1>
         <br/>
+        <button>New Applicaton</button>
         <div id="results">
           {this.state.applications.length > 0 ? this.state.applications.map((item,key)=>{
             return(
               <p key={key}>
                 <span className="applicationName">{item.application.title}</span> |
-                 <span className="responseCount">{item.responses.length} Responses!</span> |
-                 <button>Delete</button> |
+                <span className="responseCount">{item.responses.length} Responses!</span> |
+                 <button id={item.application.id} data-id={key} onClick={e=>{this.delete(item.application.id, key)}} >Delete</button> |
                  <button>View Responses</button> |
                  <button onClick={e=> location="/application/"+item.application.id }>Visit</button>
               </p>
