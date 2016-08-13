a1c556c32c6556ad4b99cd01fe103f350ebfc8ee
Fizal Sarif <fizal.sarif@gmail.com>
2016-08-13 15:53:56 -0400

    dynamic charts

--- src/client/app/dashboard/Active.jsx
+++ src/client/app/dashboard/Active.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
-
+import React    from 'react'
+import {RadarChart, Radar, PolarGrid, PolarAngleAxis, PolarRadiusAxis} from 'recharts'
 const Active =props=>{
+
+  const genChart=(index)=>{
+    index = JSON.parse(index).tree.children[0].children[0].children
+    let data = []
+    let labels=["A", "B", "C", "D", "E"]
+
+    index.forEach((item, key)=>{
+      data.push({"attribute": labels[key], "%": Math.abs(item.percentage)})
+    })
+
+    return(
+        <RadarChart cx={220} cy={200} outerRadius={150} width={400} height={400} data={data}>
+          <Radar dataKey="%" stroke="#fff" fill="#8884d8" fillOpacity={0.6}/>
+          <PolarGrid />
+          <PolarAngleAxis dataKey="attribute" />
+          <PolarRadiusAxis/>
+        </RadarChart>
+      )
+  }
+
+
   return(
       <div>
         <h3>{props.application.application.title}</h3>
@@ -8,6 +29,10 @@ const Active =props=>{
           return(
             <div key={key} className="card">
               <div className="card-block">
+                {genChart(response.personality_index)}
+                <p className="card-text">
+                A - Openness, B - Conscientiousness, C - Extraversion, D - Agreeableness, E - Emotional range
+                </p>
                 <h4 className="card-title">{response.name}</h4>
                 <p className="card-text">{response.email}</p>
               </div>
